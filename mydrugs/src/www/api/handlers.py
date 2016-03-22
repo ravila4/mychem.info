499b57e71f73e59820bb850c432f6a208c9aa81b
Julee  Adesara <jadesara@su01.scripps.edu>
2016-03-22 11:09:46 -0700

    initial commit

---
+++ mydrugs/src/www/api/handlers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from biothings.www.api.handlers import MetaDataHandler, BiothingHandler, QueryHandler, StatusHandler, FieldsHandler
+from biothings.settings import BiothingSettings
+from www.api.es import ESQuery
+import config
+
+biothing_settings = BiothingSettings()
+
+class DrugsHandler(BiothingHandler):
+    ''' This class is for the /drugs endpoint. '''
+    esq = ESQuery()
+
+class QueryHandler(QueryHandler):
+    ''' This class is for the /query endpoint. '''
+    esq = ESQuery()
+
+class StatusHandler(StatusHandler):
+    ''' This class is for the /status endpoint. '''
+    esq = ESQuery()
+
+class FieldsHandler(FieldsHandler):
+    ''' This class is for the /metadata/fields endpoint. '''
+    esq = ESQuery()
+
+class MetaDataHandler(MetaDataHandler):
+    ''' This class is for the /metadata endpoint. '''
+    esq = ESQuery()
+
+
+def return_applist():
+    ret = [
+        (r"/status", StatusHandler),
+        (r"/metadata", MetaDataHandler),
+        (r"/metadata/fields", FieldsHandler),
+    ]
+    if biothing_settings._api_version:
+        ret += [
+            (r"/" + biothing_settings._api_version + "/metadata", MetaDataHandler),
+            (r"/" + biothing_settings._api_version + "/metadata/fields", FieldsHandler),
+            (r"/" + biothing_settings._api_version + "/drugs/(.+)/?", DrugsHandler),
+            (r"/" + biothing_settings._api_version + "/drugs/?$", DrugsHandler),
+            (r"/" + biothing_settings._api_version + "/query/?", QueryHandler),
+        ]
+    else:
+        ret += [
+            (r"/drugs/(.+)/?", DrugsHandler),
+            (r"/drugs/?$", DrugsHandler),
+            (r"/query/?", QueryHandler),
+        ]
+    return ret
