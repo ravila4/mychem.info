3a08080cb02ab5b9a1baede0f139d5b74d4931f1
Julee  Adesara <jadesara@su01.scripps.edu>
2016-05-27 09:22:49 -0700

    import collections for OrderedDict

--- mydrugs/src/utils/dataload.py
+++ mydrugs/src/utils/dataload.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 #from __future__ import unicode_literals
 import itertools
 import csv
+import collections
 from utils.common import open_anyfile, is_str
 """
 Utility functions for parsing flatfiles,
