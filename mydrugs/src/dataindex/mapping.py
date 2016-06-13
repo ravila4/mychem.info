de3f13ad4f5122f91375935a1aaa6ba6f3635018
Julee  Adesara <jadesara@su01.scripps.edu>
2016-06-13 11:28:32 -0700

    added mapping of pubchem

--- mydrugs/src/dataindex/mapping.py
+++ mydrugs/src/dataindex/mapping.py
@@ -3,7 +3,7 @@ import importlib
 
 def get_mapping(sources=None):
     if sources is None:
-        sources = sources or ['drugbank', 'chembl']
+        sources = sources or ['drugbank', 'chembl','pubchem']
         extra_mapping_li = []
     else:
         extra_mapping_li = []
