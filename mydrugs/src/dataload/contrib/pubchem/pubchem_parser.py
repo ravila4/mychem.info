7f52ca316573cd34c7c055c152b85b31e8c980b0
Julee  Adesara <jadesara@su01.scripps.edu>
2016-08-31 14:14:35 -0700

    updated and python 3 compatible

--- mydrugs/src/dataload/contrib/pubchem/pubchem_parser.py
+++ mydrugs/src/dataload/contrib/pubchem/pubchem_parser.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import xmltodict
-import urllib2
+import urllib.request
+#import urllib2
 import re
 import gzip
 import os
@@ -14,31 +15,30 @@ def load_data(url):
         return True       
 
     temp_file = "temp.xml.gz"    
-    resp = urllib2.urlopen(url).read()        
+    resp = urllib.request.urlopen(url).read().decode('utf-8')        
     links = re.findall('Compound_.*.gz', resp)  #find all url from the html 
     for x in links:
         new_url = url + x    
-        f = urllib2.urlopen(new_url)   
+        f = urllib.request.urlopen(new_url)   
         data = f.read()       
-        with open(temp_file,"w") as _file:
+        with open(temp_file,"wb") as _file:
             _file.write(data)        
-        _f = gzip.open(temp_file,'r')        
-        xmltodict.parse(_f,item_depth=2,item_callback=handle)  #parse the xml file to dictionary   
+        _f = gzip.open(temp_file,'rb').read()        
+        xmltodict.parse(_f,item_depth=2,item_callback=handle,xml_attribs=True)  #parse the xml file to dictionary   
         for compound in compound_list:
             yield compound        
         os.remove(temp_file)        
-    client.close() 
 
 def restructure_dict(dictionary):
     smile_dict = dict()
     iupac_dict = dict()
     d = dict()
 
-    for key,value in dictionary.iteritems():
+    for key,value in iter(dictionary.items()):
         if key == "PC-Compound_id":
             for cnt in value:
-                for m,n in value[cnt].iteritems():
-                    for x,y in n.iteritems():
+                for m,n in iter(value[cnt].items()):
+                    for x,y in iter(n.items()):
                         d["cid"] = y                        
                         
         elif key == "PC-Compound_charge":
@@ -47,14 +47,14 @@ def restructure_dict(dictionary):
         elif key == "PC-Compound_props":
             for cnt in value:
                 for ele in value[cnt]:
-                    for x,y in ele.iteritems():
+                    for x,y in iter(ele.items()):
                         if x == "PC-InfoData_urn":
-                            for i,j in y.iteritems():                        
+                            for i,j in iter(y.items()):                        
                                 if i == "PC-Urn":
                                     val = ele["PC-InfoData_value"]
                                     for z in val:
                                         val1 = val[z]
-                                    for k,l in j.iteritems():
+                                    for k,l in iter(j.items()):
                                         if l == "Hydrogen Bond Acceptor":
                                             d["hydrogen_bond_acceptor_count"] = val1
                                         
@@ -65,7 +65,7 @@ def restructure_dict(dictionary):
                                             d["rotatable_bond_count"] = val1
                                         
                                         elif l == "IUPAC Name":                                        
-                                            IUPAC = w["PC-Urn_name"]
+                                            IUPAC = j["PC-Urn_name"]
                                             IUPAC = IUPAC.lower()
                                             iupac_dict[IUPAC] = val1
                                             d["iupac"] = iupac_dict
@@ -109,7 +109,7 @@ def restructure_dict(dictionary):
                             
         elif key == "PC-Compound_count":
             for cnt in value:
-                for x,y in value[cnt].iteritems():
+                for x,y in iter(value[cnt].items()):
                     if x == "PC-Count_heavy-atom":                    
                         d["heavy_atom_count"] = y
                     
