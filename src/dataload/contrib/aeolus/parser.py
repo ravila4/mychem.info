32165e1368f032eca76cc248df850cfadafb3525
Greg Stupp <gstupp@scripps.edu>
2017-01-16 14:21:10 -0800

    fix type

--- src/dataload/contrib/aeolus/parser.py
+++ src/dataload/contrib/aeolus/parser.py
@@ -14,7 +14,7 @@ from pymongo import MongoClient
 from local import MONGO_PASS
 
 
-def parse(aeolus, unii):
+def merge_with_unii(aeolus, unii):
     df = pd.merge(unii[['UNII', 'PT', 'RXCUI', 'INCHIKEY']], aeolus, how="right", left_on="RXCUI",
                   right_on="drug_concept_code")
     gb = df.groupby("INCHIKEY")
@@ -22,8 +22,13 @@ def parse(aeolus, unii):
     for inchikey, subdf in gb:
         dr = subdf.to_dict("records")
         dr = [{k.lower(): v for k, v in record.items() if pd.notnull(v)} for record in dr]
-        yield {'_id': inchikey, 'unii': dr}
+        yield {'_id': inchikey, 'aeolus': dr}
 
+
+def merge_with_ginas(aeolus, ginas):
+    df = pd.merge(ginas[['CAS_primary', 'RXCUI', 'UNII', 'mixture_UNII', 'preferred_names', 'substanceClass', 'inchikey']],
+                  aeolus, how="right", left_on="RXCUI",
+                  right_on="drug_concept_code")
 
 if __name__ == "__main__":
 
@@ -36,7 +41,15 @@ if __name__ == "__main__":
     unii = pd.read_csv('../unii/unii_records.tsv', sep='\t', low_memory=False, dtype=str)
 
     db = MongoClient('mongodb://mydrug_user:{}@su08.scripps.edu:27017/drugdoc'.format(MONGO_PASS)).drugdoc
+    #db = MongoClient('mongodb://mydrug_user:{}@localhost:27027/drugdoc'.format(MONGO_PASS)).drugdoc
     coll = db['aeolus']
 
-    for doc in parse(aeolus, unii):
+    for doc in merge_with_unii(aeolus, unii):
         coll.insert_one(doc)
+
+"""
+# print missing
+df[df.INCHIKEY.isnull()][['drug_concept_code', 'drug_name', 'UNII']].drop_duplicates().to_csv("missing.csv")
+
+
+"""
