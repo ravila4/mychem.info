394b55857bbe7322edac615df67790bcae031514
Greg Stupp <gstupp@scripps.edu>
2017-01-16 17:26:09 -0800

    make field names shorter, group drug fields together

--- src/dataload/contrib/aeolus/__init__.py
+++ src/dataload/contrib/aeolus/__init__.py
@@ -1,97 +1,36 @@
-import json
-
 __METADATA__ = {
     "src_name": 'AEOLUS',
     "src_url": 'http://www.nature.com/articles/sdata201626',
     "field": "aeolus"
 }
-"""
-{'case_count': 1,
-'drug_concept_code': '476818',
-'drug_concept_id': '19059006',
-'drug_name': 'Magnesium glycinate',
-'drug_vocabulary': 'RxNorm',
-'inchikey': 'AACACXATQSKRQG-UHFFFAOYSA-L',
-'outcome_concept_code': '10009033',
-'outcome_concept_id': '37219804',
-'outcome_name': 'Chronic obstructive pulmonary disease',
-'outcome_vocabulary': 'MedDRA',
-'prr': 4.03062,
-'prr_95_percent_lower_confidence_limit': 0.5713199999999999,
-'prr_95_percent_upper_confidence_limit': 28.435470000000002,
-'pt': 'MAGNESIUM GLYCINATE',
-'ror': 4.05017,
-'ror_95_percent_lower_confidence_limit': 0.5669,
-'ror_95_percent_upper_confidence_limit': 28.935840000000002,
-'rxcui': '476818',
-'snomed_outcome_concept_id': '255573',
-'unii': 'IFN18A4Y6B'}"""
+
 
 def get_mapping():
     mapping = {
         "aeolus": {
             "properties": {
-                "case_count": {
-                    "type": "long",
-                },
-                "drug_concept_code": {
+                "drug_code": {
                     "type": "string",
                     "analyzer": "string_lowercase"
                 },
-                "drug_concept_id": {
+                "drug_id": {
                     "type": "string",
                     "analyzer": "string_lowercase"
                 },
                 "drug_name": {
                     "type": "string",
                 },
-                "drug_vocabulary": {
+                "drug_vocab": {
                     "type": "string",
                 },
                 "inchikey": {
                     "type": "string",
                     "analyzer": "string_lowercase"
                 },
-                "outcome_concept_code": {
-                    "type": "string",
-                    "analyzer": "string_lowercase"
-                },
-                "outcome_concept_id": {
-                    "type": "string",
-                    "analyzer": "string_lowercase"
-                },
-                "outcome_name": {
-                    "type": "string",
-                },
-                "outcome_vocabulary": {
-                    "type": "string",
-                },
-                "prr": {
-                    "type": "float",
-                },
-                "prr_95_percent_lower_confidence_limit": {
-                    "type": "float",
-                },
-                "prr_95_percent_upper_confidence_limit": {
-                    "type": "float",
-                },
                 "pt": {
                     "type": "string",
                 },
-                "ror": {
-                    "type": "float",
-                },
-                "ror_95_percent_lower_confidence_limit": {
-                    "type": "float",
-                },
-                "ror_95_percent_upper_confidence_limit": {
-                    "type": "float",
-                },
                 "rxcui": {
-                    "type": "string",
-                    "analyzer": "string_lowercase"
-                },
-                "snomed_outcome_concept_id": {
                     "type": "string",
                     "analyzer": "string_lowercase"
                 },
@@ -99,6 +38,45 @@ def get_mapping():
                     "type": "string",
                     "analyzer": "string_lowercase"
                 },
+                "outcomes": {
+                    "properties": {
+                        "case_count": {
+                            "type": "long",
+                        },
+                        "outcome_code": {
+                            "type": "string",
+                            "analyzer": "string_lowercase"
+                        },
+                        "outcome_id": {
+                            "type": "string",
+                            "analyzer": "string_lowercase"
+                        },
+                        "outcome_name": {
+                            "type": "string",
+                        },
+                        "outcome_vocab": {
+                            "type": "string",
+                        },
+                        "prr": {
+                            "type": "float",
+                        },
+                        "prr_95_CI_lower": {
+                            "type": "float",
+                        },
+                        "prr_95_CI_upper": {
+                            "type": "float",
+                        },
+                        "ror": {
+                            "type": "float",
+                        },
+                        "ror_95_CI_lower": {
+                            "type": "float",
+                        },
+                        "ror_95_CI_upper": {
+                            "type": "float",
+                        },
+                    },
+                }
             }
         }
     }
--- src/dataload/contrib/aeolus/parser.py
+++ src/dataload/contrib/aeolus/parser.py
@@ -8,48 +8,91 @@ Then run this..
 
 """
 
+import json
+
 import pandas as pd
+import sys
 from pymongo import MongoClient
 
 from local import MONGO_PASS
 
 
 def merge_with_unii(aeolus, unii):
-    df = pd.merge(unii[['UNII', 'PT', 'RXCUI', 'INCHIKEY']], aeolus, how="right", left_on="RXCUI",
-                  right_on="drug_concept_code")
-    gb = df.groupby("INCHIKEY")
+    df = pd.merge(unii[['unii', 'pt', 'rxcui', 'inchikey']], aeolus, how="right", left_on="rxcui", right_on="drug_code")
+    gb = df.groupby("inchikey")
 
     for inchikey, subdf in gb:
-        dr = subdf.to_dict("records")
-        dr = [{k.lower(): v for k, v in record.items() if pd.notnull(v)} for record in dr]
-        yield {'_id': inchikey, 'aeolus': dr}
+        subdf_records = subdf[['ror', 'prr', 'prr_95_CI_lower', 'prr_95_CI_upper', 'ror_95_CI_lower', 'ror_95_CI_upper',
+                               'outcome_vocab', 'case_count', 'outcome_code', 'outcome_id', 'outcome_name']]
+        top_level_df = subdf[['unii', 'drug_code', 'drug_name', 'drug_vocab', 'inchikey', 'drug_id', 'rxcui', 'pt']].drop_duplicates()
+        if len(top_level_df) != 1:
+            raise ValueError(top_level_df)
+
+        top_level = dict(top_level_df.iloc[0])
+        dr = subdf_records.to_dict("records")
+        dr = [{k: v for k, v in record.items() if pd.notnull(v)} for record in dr]
+        dr.sort(key=lambda x: x['case_count'])
+        top_level['outcomes'] = dr
+        yield {'_id': inchikey, 'aeolus': top_level}
 
 
 def merge_with_ginas(aeolus, ginas):
-    df = pd.merge(ginas[['CAS_primary', 'RXCUI', 'UNII', 'mixture_UNII', 'preferred_names', 'substanceClass', 'inchikey']],
-                  aeolus, how="right", left_on="RXCUI",
-                  right_on="drug_concept_code")
+    df = pd.merge(
+        ginas[['CAS_primary', 'RXCUI', 'UNII', 'mixture_UNII', 'preferred_names', 'substanceClass', 'inchikey']],
+        aeolus, how="right", left_on="RXCUI",
+        right_on="drug_concept_code")
 
-if __name__ == "__main__":
 
+def main():
     aeolus = pd.read_csv('aeolus.tsv', sep='\t', low_memory=False,
                          dtype={'drug_concept_id': str, 'outcome_concept_id': str,
                                 'drug_concept_code': str,
                                 'outcome_concept_code': str,
                                 'snomed_outcome_concept_id': str})
 
+    aeolus.rename(columns={'prr_95_percent_lower_confidence_limit': 'prr_95_CI_lower',
+                           'prr_95_percent_upper_confidence_limit': 'prr_95_CI_upper',
+                           'ror_95_percent_lower_confidence_limit': 'ror_95_CI_lower',
+                           'ror_95_percent_upper_confidence_limit': 'ror_95_CI_upper',
+                           'outcome_concept_code': 'outcome_code',
+                           'outcome_concept_id': 'outcome_id',
+                           'drug_concept_code': 'drug_code',
+                           'drug_concept_id': 'drug_id',
+                           'outcome_vocabulary': 'outcome_vocab',
+                           'drug_vocabulary': 'drug_vocab',
+                           }, inplace=True)
+
     unii = pd.read_csv('../unii/unii_records.tsv', sep='\t', low_memory=False, dtype=str)
+    unii.columns = unii.columns.str.lower()
 
+    i = merge_with_unii(aeolus, unii)
+
+    with open("aeolus.json", "w") as f:
+        for doc in i:
+            print(json.dumps(doc), file=f)
+
+
+def insert_mongo():
     db = MongoClient('mongodb://mydrug_user:{}@su08.scripps.edu:27017/drugdoc'.format(MONGO_PASS)).drugdoc
-    #db = MongoClient('mongodb://mydrug_user:{}@localhost:27027/drugdoc'.format(MONGO_PASS)).drugdoc
+    # db = MongoClient('mongodb://mydrug_user:{}@localhost:27027/drugdoc'.format(MONGO_PASS)).drugdoc
     coll = db['aeolus']
+    coll.drop()
 
-    for doc in merge_with_unii(aeolus, unii):
-        coll.insert_one(doc)
+    docs = open("aeolus.json")
+
+    for doc in docs:
+        coll.insert_one(json.loads(doc))
+
+
+if __name__ == "__main__":
+    main()
+
+    if len(sys.argv) == 2 and sys.argv[1] == "mongo":
+        insert_mongo()
 
 """
 # print missing
-df[df.INCHIKEY.isnull()][['drug_concept_code', 'drug_name', 'UNII']].drop_duplicates().to_csv("missing.csv")
+df[df.INCHIKEY.isnull()][['drug_concept_code', 'drug_name', 'UNII', 'RXCUI']].drop_duplicates().to_csv("missing.csv")
 
 
 """
