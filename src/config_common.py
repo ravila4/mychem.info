98412a8797a2fbadc7905f62fa82059da482bbbb
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-07-05 17:38:02 +0000

    Adding /chem as an alias for mychem, committing some old changes for id lookup regex

--- src/config_common.py
+++ src/config_common.py
@@ -32,6 +32,8 @@ APP_LIST = [
     (r"/{}/drug/?$".format(API_VERSION), DrugHandler),
     (r"/{}/compound/(.+)/?".format(API_VERSION), DrugHandler),
     (r"/{}/compound/?$".format(API_VERSION), DrugHandler),
+    (r"/{}/chem/(.+)/?".format(API_VERSION), DrugHandler),
+    (r"/{}/chem/?$".format(API_VERSION), DrugHandler),
     (r"/{}/query/?".format(API_VERSION), QueryHandler),
     (r"/{}/metadata/?".format(API_VERSION), MetadataHandler),
     (r"/{}/metadata/fields/?".format(API_VERSION), MetadataHandler),
@@ -62,7 +64,7 @@ GA_ACTION_ANNOTATION_GET = 'drug_get'
 GA_ACTION_ANNOTATION_POST = 'drug_post'
 GA_TRACKER_URL = 'c.biothings.io'
 
-ANNOTATION_ID_REGEX_LIST = [(re.compile(r'db[0-9]+', re.I), 'drugbank.drugbank_id'),
+ANNOTATION_ID_REGEX_LIST = [(re.compile(r'db[0-9]+', re.I), ['drugbank.drugbank_id','drugbank.accession_number']),
                             (re.compile(r'chembl[0-9]+', re.I), 'chembl.molecule_chembl_id'),
                             (re.compile(r'chebi\:[0-9]+', re.I), 'chebi.chebi_id'),
                             (re.compile(r'cid[0-9]+', re.I), 'pubchem.cid'),
