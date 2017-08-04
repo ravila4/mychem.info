d73cdfaf7d1eda4fd4d43a1e0281c72434fc07f6
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-08-03 18:39:00 -0700

    Adding license to mychem API

--- src/web/api/transform.py
+++ src/web/api/transform.py
@@ -3,4 +3,17 @@ from biothings.web.api.es.transform import ESResultTransformer
 
 class ESResultTransformer(ESResultTransformer):
     # Add app specific result transformations
-    pass
+    def _modify_doc(self, doc):
+        for source, val in self.source_metadata.items():
+            if source in doc:
+                if isinstance(doc[source], dict):
+                    try:
+                        doc[source]['_license'] = val.get('license_url_short', val.get('license_url'))
+                    except:
+                        pass
+                elif isinstance(doc[source], list):
+                    for d in doc[source]:
+                        try:
+                            d['_license'] = val.get('license_url_short', val.get('license_url'))
+                        except:
+                            pass
