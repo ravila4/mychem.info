32b2e64f34a9a0d0d128dc7def596ab9d22638de
kevinxin90 <kevinxin@scripps.edu>
2018-06-29 16:03:29 -0700

    add file path

--- src/hub/dataload/sources/drugcentral/__init__.py
+++ src/hub/dataload/sources/drugcentral/__init__.py
@@ -1,18 +1,18 @@
 
-file_path_pharma_class = '/Users/kevinxin/Documents/myvariant.info/mychem/data/pharma_class.csv'
+file_path_pharma_class = '/home/kevinxin/drug/drugcentral/2018.6/pharma_class.csv'
 #df_drugcentral_pharma_class = pd.read_csv(file_path_pharma_class, sep=",", names=['_id', 'struc_id', 'role', 'description', 'code', 'source'])
-file_path_faers = '/Users/kevinxin/Documents/myvariant.info/mychem/data/faers.csv'
+file_path_faers = '/home/kevinxin/drug/drugcentral/2018.6/faers.csv'
 #df_drugcentral_faers = pd.read_csv(file_path_faers, sep=",", names=['_id', 'struc_id', 'meddra_term', 'meddra_code', 'level', 'llr', 'llr_threshold', 'drug_ae', 'drug_no_ae', 'no_drug_ae', 'no_drug_no_ar'])
-file_path_act = '/Users/kevinxin/Documents/myvariant.info/mychem/data/act_table_full.csv'
+file_path_act = '/home/kevinxin/drug/drugcentral/2018.6/act_table_full.csv'
 #df_drugcentral_act = pd.read_csv(file_path_act, sep=",", names=["act_id", "struct_id", "target_id", "target_name", "target_class", "accession", "gene", "swissprot", "act_value", "act_unit", "act_type", "act_comment", "act_source", "relation", "moa", "moa_source", "act_source_url", "moa_source_url", "action_type", "first_in_class", "tdl", "act_ref_id", "moa_ref_id", "organism"])
-file_path_omop = '/Users/kevinxin/Documents/myvariant.info/mychem/data/omop_relationship.csv'
+file_path_omop = '/home/kevinxin/drug/drugcentral/2018.6/omop_relationship.csv'
 #df_omop = pd.read_csv(file_path_omop, sep=",", names=['_id', 'struct_id', 'concept_id', 'relationship_name', 'concept_name', 'umls_cui', 'snomed_full_name', 'cui_semantic_type', 'snomed_conceptid'])
-file_path_approval = '/Users/kevinxin/Documents/myvariant.info/mychem/data/approval.csv'
+file_path_approval = '/home/kevinxin/drug/drugcentral/2018.6/approval.csv'
 #df_drug_approval = pd.read_csv(file_path_approval, sep=",", names=['_id', 'struct_id', 'approval', 'type', 'applicant', 'orphan'])
-file_path_synonym = '/Users/kevinxin/Documents/myvariant.info/mychem/data/synonyms.csv'
+file_path_synonym = '/home/kevinxin/drug/drugcentral/2018.6/synonyms.csv'
 #df_synonym = pd.read_csv(file_path_synonym, sep=",", names=["_id", "struct_id", 'synonym', 'pref', 'parent', 's2'])
-file_path_structure = '/Users/kevinxin/Documents/myvariant.info/mychem/data/structures.smiles.tsv'
+file_path_structure = '/home/kevinxin/drug/drugcentral/2018.6/structures.smiles.tsv'
 #df_structure = pd.read_csv(file_path_structure, sep="\t")
-file_path_identifier = '/Users/kevinxin/Documents/myvariant.info/mychem/data/identifiers.csv'
+file_path_identifier = '/home/kevinxin/drug/drugcentral/2018.6/identifiers.csv'
 #df_identifier = pd.read_csv(file_path_identifiers, sep=",", names=["_id", "identifier", "id_type", "struct_id", "parent"])
-file_path_drug_dosage = '/Users/kevinxin/Documents/myvariant.info/mychem/data/drug_dosage.csv'
+file_path_drug_dosage = '/home/kevinxin/drug/drugcentral/2018.6/drug_dosage.csv'
