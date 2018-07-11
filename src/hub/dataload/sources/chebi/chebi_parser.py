2c4a394a1a6cce80c88148b3a5cea0fff4eef09a
kevinxin90 <kevinxin@scripps.edu>
2018-07-11 10:45:28 -0700

    add value convert to number for pubchem_database_link

--- src/hub/dataload/sources/chebi/chebi_parser.py
+++ src/hub/dataload/sources/chebi/chebi_parser.py
@@ -46,7 +46,7 @@ def restructure_dict(dictionary):
     restr_dict['chebi'] = clean_up(restr_dict['chebi'])
     restr_dict = dict_sweep(restr_dict,vals=[None,".", "-", "", "NA", "none", " ", "Not Available",
         "unknown","null","None","NaN"])
-    restr_dict = value_convert_to_number(unlist(restr_dict),skipped_keys=["beilstein_registry_numbers","pubchem_database_links","pubmed_citation_links","sabio_rk_database_links","gmelin_registry_numbers","molbase_database_links"])
+    restr_dict = value_convert_to_number(unlist(restr_dict),skipped_keys=["beilstein_registry_numbers","pubmed_citation_links","sabio_rk_database_links","gmelin_registry_numbers","molbase_database_links"])
     return restr_dict
 
 def find_inchikey(doc, drugbank_col, chembl_col):
