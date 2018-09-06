8a164d18794867f3c528716df9dcff7971cf421f
kevinxin90 <kevinxin@scripps.edu>
2018-09-06 13:49:40 -0700

    update chembl parser to change cross reference structure

--- src/hub/dataload/sources/chembl/chembl_parser.py
+++ src/hub/dataload/sources/chembl/chembl_parser.py
@@ -1,6 +1,6 @@
 import urllib.request
 import json
-import collections
+from collections import defaultdict
 from biothings.utils.dataload import dict_sweep, unlist, value_convert_to_number
 from biothings.utils.dataload import boolean_convert
 
@@ -17,10 +17,32 @@ def load_data(input_file):
             pass
         yield restr_dict
 
+def restructure_xref(xref_list):
+    """
+    Group the cross references field based on the source
+    Also change the field name
+    """
+    xref_output = defaultdict(list)
+    for _record in xref_list:
+        if 'xref_src' in _record and _record['xref_src'] == 'PubChem':
+            assert _record['xref_name'].startswith('SID: ')
+            xref_output['pubchem'].append({'sid': int(_record['xref_id'])})
+        elif 'xref_src' in _record and _record['xref_src'] == 'Wikipedia':
+            xref_output['wikipedia'].append({'url_stub': _record['xref_id']})
+        elif 'xref_src' in _record and _record['xref_src'] == 'TG-GATEs':
+            xref_output['tg-gates'].append({'name': _record['xref_name'], 'id': int(_record['xref_id'])})
+        elif 'xref_src' in _record and _record['xref_src'] == 'DailyMed':
+            xref_output['dailymed'].append({'name': _record['xref_name']})
+        elif 'xref_src' in _record and _record['xref_src'] == 'DrugCentral':
+            xref_output['drugcentral'].append({'name': _record['xref_name'], 'id': int(_record['xref_id'])})
+    return xref_output
+
+
 def restructure_dict(dictionary):
     restr_dict = dict()
     _flag = 0
     for key in list(dictionary): # this is for 1
+        print(key)
         if key == 'molecule_chembl_id':
             restr_dict['_id']=dictionary[key]
         if key == 'molecule_structures' and type(dictionary['molecule_structures'])==dict:
@@ -37,7 +59,11 @@ def restructure_dict(dictionary):
 
     if _flag == 0:
         restr_dict['chembl'] = dictionary
+    if 'cross_references' in restr_dict['chembl'] and restr_dict['chembl']['cross_references']:
+        restr_dict['chembl']['xref'] = restructure_xref(restr_dict['chembl']['cross_references'])
+
     del restr_dict['chembl']['molecule_structures']
+    del restr_dict['chembl']['cross_references']
     restr_dict = unlist(restr_dict)
     # Add "CHEBI:" prefix, standardize the way representing CHEBI IDs
     if 'chebi_par_id' in restr_dict['chembl']:
@@ -47,7 +73,4 @@ def restructure_dict(dictionary):
     restr_dict = boolean_convert(restr_dict, ["topical","oral","parenteral","dosed_ingredient","polymer_flag",
         "therapeutic_flag","med_chem_friendly","molecule_properties.ro3_pass"])
     return restr_dict
-
-
-
 
