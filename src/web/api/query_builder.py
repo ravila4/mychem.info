f3bce15117bb034863b32158a0f8301085e20c60
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2018-10-22 08:23:27 -0700

    Updating query builder and transform to deal with script_fields

--- src/web/api/query_builder.py
+++ src/web/api/query_builder.py
@@ -3,4 +3,23 @@ from biothings.web.api.es.query_builder import ESQueryBuilder
 
 class ESQueryBuilder(ESQueryBuilder):
     # Implement app specific queries here
-    pass
+    def _build_single_query(self, term, scopes=None):
+        scopes = scopes or self.default_scopes
+        if len(scopes) == 1:
+            _query = self.queries.match({scopes[0]:{"query": "{}".format(term), "operator": "and"}})
+        else:
+            _query = self.queries.multi_match({"query":"{}".format(term), "fields":scopes, "operator":"and"})
+        _query['script_fields'] = {'_source': {'script': {'id': 'truncate-large-fields'}}}
+        return _query
+
+    def add_extra_filters(self, q):
+        ''' Override me to add more filters '''
+        q['script_fields'] = {'_source': {'script': {'id': 'truncate-large-fields'}}}
+        return q
+
+    def _annotation_GET_query(self, bid):
+        # don't go to .get here, still make a match _id query
+        _scopes = self._get_term_scope(bid)
+        #if not _scopes:
+        #    _scopes = ['_id']
+        return self._return_query_kwargs({'body': self._build_single_query(bid, scopes=_scopes)})
--- src/web/api/transform.py
+++ src/web/api/transform.py
@@ -3,6 +3,12 @@ from biothings.web.api.es.transform import ESResultTransformer
 
 class ESResultTransformer(ESResultTransformer):
     # Add app specific result transformations
+    def _get_doc(self, doc):
+        _doc = doc.get('_source', doc.get('fields', {}))
+        if "_source" in _doc:
+            _doc = _doc['_source'][0]
+        return _doc
+
     def _modify_doc(self, doc):
         for source, val in self.source_metadata.items():
             if source in doc:
