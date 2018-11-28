3e5233ad43e1529c391a46d30ff589ab3a380975
Greg Taylor <greg.k.taylor@gmail.com>
2018-11-28 14:32:18 -0800

    datatransform - add mychem_api_graph for keylookup

---
+++ src/hub/datatransform/mychem_api_graph.py
@@ -0,0 +1,76 @@
+from biothings.hub.datatransform import DataTransformMDB
+from biothings.hub.datatransform.datatransform_api import MyChemInfoEdge
+import networkx as nx
+
+
+graph_mychem = nx.DiGraph()
+
+###############################################################################
+# PharmGKB Nodes and Edges
+###############################################################################
+graph_mychem.add_node('inchi')
+graph_mychem.add_node('chebi')
+graph_mychem.add_node('chembl')
+graph_mychem.add_node('drugbank')
+graph_mychem.add_node('drugname')
+graph_mychem.add_node('pubchem')
+graph_mychem.add_node('rxnorm')
+graph_mychem.add_node('unii')
+graph_mychem.add_node('inchikey')
+graph_mychem.add_node('pharmgkb')
+
+################################################################################
+# MyChem.Info API based lookup
+################################################################################
+
+graph_mychem.add_edge('drugbank', 'pubchem',
+                      object=MyChemInfoEdge('drugbank.drugbank_id', 'pubchem.cid'))
+
+graph_mychem.add_edge('pharmgkb', 'drugbank',
+                      object=MyChemInfoEdge('pharmgkb.id', 'pharmgkb.xrefs.drugbank'))
+
+
+####################
+# Inchi
+####################
+
+graph_mychem.add_edge('inchi', 'pubchem',
+                      object=MyChemInfoEdge('pubchem.inchi', 'pubchem.cid', weight=1.0))
+
+graph_mychem.add_edge('inchi', 'drugbank',
+                      object=MyChemInfoEdge('drugbank.inchi', 'drugbank.drugbank_id', weight=1.1))
+
+graph_mychem.add_edge('inchi', 'chembl',
+                      object=MyChemInfoEdge('chembl.inchi', 'chembl.molecule_chembl_id', weight=1.2))
+
+####################
+# InchiKey
+####################
+inchikey_fields = [
+    'pubchem.inchi_key',
+    'drugbank.inchi_key',
+    'chembl.inchi_key'
+]
+
+graph_mychem.add_edge('pubchem', 'inchikey',
+                      object=MyChemInfoEdge('pubchem.cid', inchikey_fields))
+
+graph_mychem.add_edge('drugbank', 'inchikey',
+                      object=MyChemInfoEdge('drugbank.drugbank_id', inchikey_fields))
+
+graph_mychem.add_edge('chembl', 'inchikey',
+                      object=MyChemInfoEdge('chembl.molecule_chembl_id', inchikey_fields))
+
+# self-loops to check looked-up values exist in official collection
+graph_mychem.add_edge('drugbank', 'drugbank',
+                      object=MyChemInfoEdge('drugbank.drugbank_id', 'drugbank.drugbank_id'))
+
+
+class MyChemKeyLookup(DataTransformMDB):
+
+    def __init__(self, input_types, *args, **kwargs):
+        super(MyChemKeyLookup, self).__init__(graph_mychem,
+                input_types,
+                output_types=['inchikey', 'unii', 'rxnorm', 'drugbank',
+                              'chebi', 'chembl', 'pubchem', 'drugname'],
+                *args, **kwargs)
