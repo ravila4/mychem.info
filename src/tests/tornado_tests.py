ef9b6d7f0cadb63536a21cfb6c6e3e9d04bd67a2
Jerry <xzhou@scripps.edu>
2019-02-23 18:59:35 -0800

    Complies with new biothings test module

--- src/tests/tornado_tests.py
+++ src/tests/tornado_tests.py
@@ -1,25 +1,28 @@
-import sys
-import os
+''' MyChem Data-Aware Tests
+    nosetests tornado_tests
+    nosetests tornado_tests:MyChemTestTornadoClient
+'''
 
-src_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
-if src_path not in sys.path:
-    sys.path.insert(0, src_path)
-
-from tornado.testing import AsyncHTTPTestCase
 from tornado.web import Application
-from tests.tests import MyChemTest
-from biothings.tests.test_helper import TornadoRequestHelper
-from web.settings import MyDrugWebSettings
-#import unittest
 
-class MyChemTornadoTest(AsyncHTTPTestCase, MyChemTest):
-    def __init__(self, methodName='runTest', **kwargs):
-        super(AsyncHTTPTestCase, self).__init__(methodName, **kwargs)
-        self.h = TornadoRequestHelper(self)
-        self._settings = MyDrugWebSettings(config='config')
+from biothings.tests.test_helper import TornadoTestServerMixin
+from tests.tests import MyChemTest
+from web.settings import MyDrugWebSettings
+
+
+class MyChemTestTornadoClient(TornadoTestServerMixin, MyChemTest):
+    '''
+        Self contained test class, can be used for CI tools such as Travis
+        Starts a Tornado server on its own and perform tests against this server.
+    '''
+    __test__ = True
+
+    @classmethod
+    def setup_class(cls):
+        ''' Reads Tornado Settings from config.py '''
+        cls.WEB_SETTINGS = MyDrugWebSettings(config='config')
+        cls.APP_LIST = cls.WEB_SETTINGS.generate_app_list()
+        cls.STATIC_PATH = cls.WEB_SETTINGS.STATIC_PATH
 
     def get_app(self):
-        return Application(self._settings.generate_app_list())
-
-#if __name__ == "__main__":
-#    unittest.TextTestRunner().run(MyChemTornadoTest.suite())
+        return Application(self.APP_LIST, static_path=self.STATIC_PATH)
