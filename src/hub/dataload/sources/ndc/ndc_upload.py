6434513adb846342cb53e883e6f58c0684ae7ca7
Greg Taylor <gregktaylor@outlook.com>
2019-03-25 20:35:47 +0000

    ndc_upload.py - simplify upload and use RootKeyMergerStorage

--- src/hub/dataload/sources/ndc/ndc_upload.py
+++ src/hub/dataload/sources/ndc/ndc_upload.py
@@ -22,32 +22,17 @@ SRC_META = {
 class NDCUploader(BaseDrugUploader):
 
     name = "ndc"
-    storage_class = (storage.BasicStorage,storage.CheckSizeStorage)
+    storage_class = (storage.RootKeyMergerStorage, storage.CheckSizeStorage)
     __metadata__ = {"src_meta" : SRC_META}
     keylookup = MyChemKeyLookup([
         ("ndc","ndc.productndc"),
-        ("drugname", "ndc.nonproprietaryname")]
-        )
+        ("drugname", "ndc.nonproprietaryname")
+        ])
     # See the comment on the ExcludeFieldsById for use of this class.
     exclude_fields = ExcludeFieldsById(exclusion_ids, ["ndc"])
 
     def load_data(self,data_folder):
-        docs = self.exclude_fields(self.keylookup(load_data))(data_folder)
-        inchi_key = {}
-        for doc in docs:
-            # IK found, but other productndc could also match the same
-            # IK so we keep them in a list
-            if type(doc["ndc"]) == list:
-                inchi_key.setdefault(doc["_id"],doc["ndc"])
-            else:
-                if not doc["ndc"] in inchi_key.setdefault(doc["_id"],[]):
-                    inchi_key.setdefault(doc["_id"],[]).append(doc["ndc"])
-        l = []
-        for ik,ndc in inchi_key.items():
-            if len(ndc) == 1:
-                ndc = ndc.pop()
-            yield {"_id": ik, "ndc": ndc}
-
+        return self.exclude_fields(self.keylookup(load_data))(data_folder)
 
     @classmethod
     def get_mapping(klass):
