07f7cad106d1b0e8fef3df2ac90af966589ce940
Greg Taylor <gregktaylor@outlook.com>
2019-03-27 17:09:28 +0000

    aeolus_dump.py - conform to linting rules

--- src/hub/dataload/sources/aeolus/aeolus_dump.py
+++ src/hub/dataload/sources/aeolus/aeolus_dump.py
@@ -1,12 +1,12 @@
+"""
+AeolusDumper - placeholder dumper for Aeolus
+"""
+# pylint: disable=E0401, E0611, C0412, C0413
 import os
 import os.path
-import sys
-import time
-import bs4
-
-import biothings, config
+import biothings
+import config
 biothings.config_for_app(config)
-
 from config import DATA_ARCHIVE_ROOT
 from biothings.hub.dataload.dumper import DummyDumper
 
@@ -15,6 +15,6 @@ class AeolusDumper(DummyDumper):
     """
     Aeolus dumper - a placeholder because the source is already in MongoDB
     """
+    # pylint: disable=R0903
     SRC_NAME = "aeolus_dt"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
-
