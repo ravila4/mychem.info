e1c393d508c31b6f6270faad2031b3c4a8bca554
Greg Taylor <gregktaylor@outlook.com>
2019-03-27 16:55:13 +0000

    drugcentral_dump.py - conform to pylint rules

--- src/hub/dataload/sources/drugcentral/drugcentral_dump.py
+++ src/hub/dataload/sources/drugcentral/drugcentral_dump.py
@@ -1,20 +1,20 @@
+"""
+Aeolus Dumper - placeholder
+"""
+# pylint: disable=E0401, E0611, C0412, C0413
 import os
 import os.path
-import sys
-import time
-import bs4
-
-import biothings, config
+import biothings
+import config
 biothings.config_for_app(config)
-
-from config import DATA_ARCHIVE_ROOT
 from biothings.hub.dataload.dumper import DummyDumper
+from config import DATA_ARCHIVE_ROOT
 
 
 class AeolusDumper(DummyDumper):
     """
     Drugcentral dumper - a placeholder because the source is already in MongoDB
     """
+    # pylint: disable=R0903
     SRC_NAME = "drugcentral_dt"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
-
