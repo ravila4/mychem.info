6dc553fe1884f4fc660187419c1c8ff3db7940d0
jiwen xin <kevinxin@scripps.edu>
2020-07-31 15:55:29 -0700

    update unichem name

--- src/hub/dataload/sources/unichem/dump.py
+++ src/hub/dataload/sources/unichem/dump.py
@@ -1,19 +1,18 @@
+from biothings.hub.dataload.dumper import FTPDumper, DumperException
+from biothings.utils.common import uncompressall
+from config import DATA_ARCHIVE_ROOT
 import os
 import time
 
-import biothings, config
+import biothings
+import config
 biothings.config_for_app(config)
 
-from config import DATA_ARCHIVE_ROOT
-
-from biothings.utils.common import uncompressall
-
-from biothings.hub.dataload.dumper import FTPDumper, DumperException
 
 class Unichem_biothings_sdkDumper(FTPDumper):
 
-    SRC_NAME = "UniChem_BioThings_SDK"
-    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)  
+    SRC_NAME = "unichem"
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     FTP_HOST = 'ftp.ebi.ac.uk'
     CWD_DIR = '/pub/databases/chembl/UniChem/data/oracleDumps'
     SCHEDULE = "0 6 * * *"
@@ -36,7 +35,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
         """Determine if newest release needs to be downloaded"""
         # try checking release of version already downloaded
         try:
-            current_release = self.src_doc.get("download",{}).get("release")
+            current_release = self.src_doc.get("download", {}).get("release")
         except:
             # set current_release to false if it will be first download
             current_release = False
@@ -50,17 +49,18 @@ class Unichem_biothings_sdkDumper(FTPDumper):
     def create_todump_list(self, force=False):
         """Add files to dump list for downloading"""
         self.get_newest_info()
-        for fn in ["UC_SOURCE.txt.gz","UC_STRUCTURE.txt.gz","UC_XREF.txt.gz"]:
-            local_file = os.path.join(self.new_data_folder,fn)
-            # add file to dump list if forced download, if path to local file doesnt exist, 
+        for fn in ["UC_SOURCE.txt.gz", "UC_STRUCTURE.txt.gz", "UC_XREF.txt.gz"]:
+            local_file = os.path.join(self.new_data_folder, fn)
+            # add file to dump list if forced download, if path to local file doesnt exist,
             # or if there is a new release available
             if force or not os.path.exists(local_file) or self.new_release_available():
-                path = "/pub/databases/chembl/UniChem/data/oracleDumps/UDRI" + self.release + "/" + fn;
-                self.to_dump.append({"remote": path, "local":local_file})
+                path = "/pub/databases/chembl/UniChem/data/oracleDumps/UDRI" + self.release + "/" + fn
+                self.to_dump.append({"remote": path, "local": local_file})
 
     def post_dump(self, *args, **kwargs):
         """After download/dump, uncompress the downloaded .gz files"""
         # UNCOMPRESS set to True
         if self.__class__.UNCOMPRESS:
-            self.logger.info("Uncompress all archive files in '%s'" % self.new_data_folder)
+            self.logger.info("Uncompress all archive files in '%s'" %
+                             self.new_data_folder)
             uncompressall(self.new_data_folder)
