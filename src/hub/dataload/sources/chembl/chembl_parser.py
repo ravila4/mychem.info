278b6d5ecaadae137a0a0e1fd37b614d2a941eb5
Ricardo Avila <ravila@protonmail.com>
2020-10-28 10:38:37 -0700

    Create functions for parsing new dumps

--- src/hub/dataload/sources/chembl/chembl_parser.py
+++ src/hub/dataload/sources/chembl/chembl_parser.py
@@ -1,21 +1,104 @@
-import urllib.request
+import os
 import json
 from collections import defaultdict
 from biothings.utils.dataload import dict_sweep, unlist, value_convert_to_number
 from biothings.utils.dataload import boolean_convert
 
-def load_data(input_file):
-    molecules_list = []
-    data = json.load(open(input_file))
-    molecules_list = data['molecules']
-    for i in range(0,len(molecules_list)):
+
+def load_data(data_folder):
+    # Data paths
+    molecules_file = os.path.join(data_folder, "molecules.json")
+    activities_file = os.path.join(data_folder, "activities.json")
+    drug_indications_file = os.path.join(data_folder, "drug_indications.json")
+    mechanisms_file = os.path.join(data_folder, "mechanisms.json")
+    metabolisms_file = os.path.join(data_folder, "metabolisms.json")
+    # Parse files
+    molecules_list = json.load(open(molecules_file))['molecules']
+    activities = json.load(open(activities_file))['activities']
+    activities = restructure_activities(activities)
+    drug_indications = json.load(open(drug_indications_file))['drug_indications']
+    drug_indications = restructure_drug_indications(drug_indications)
+    mechanisms = json.load(open(mechanisms_file))['mechanisms']
+    mechanisms = restructure_mechanisms(mechanisms)
+    metabolisms = json.load(open(metabolisms_file))['metabolisms']
+    metabolisms = restructure_metabolisms(metabolisms)
+    for i in range(0, len(molecules_list)):
         restr_dict = restructure_dict(molecules_list[i])
         try:
             _id = restr_dict["chembl"]['inchi_key']
             restr_dict["_id"] = _id
         except KeyError:
             pass
+
+        # Add additional metadata
+        chembl_id = restr_dict['chembl']["molecule_chembl_id"]
+        # Activities
+        try:
+            restr_dict["activities"] = activities[chembl_id]
+            print(json.dumps(restr_dict, indent=2))
+        except KeyError:
+            pass
+        # Drug interactions
+        try:
+            restr_dict["drug_indications"] = drug_indications[chembl_id]
+        except KeyError:
+            pass
+        # Mechanisms
+        try:
+            restr_dict["mechanisms"] = mechanisms[chembl_id]
+        except KeyError:
+            pass
+        # Metabolisms
+        try:
+            restr_dict["metabolisms"] = metabolisms[chembl_id]
+        except KeyError:
+            pass
         yield restr_dict
+
+
+def restructure_activities(activity_data):
+    """ Group activities by molecule_chembl_id
+    """
+    restr_dict = {}
+    for doc in activity_data:
+        key = doc["molecule_chembl_id"]
+        restr_dict.setdefault(key, []).append(doc)
+    restr_dict = unlist(restr_dict)
+    return restr_dict
+
+
+def restructure_drug_indications(indication_data):
+    """ Group drug indications by molecule_chembl_id
+    """
+    restr_dict = {}
+    for doc in indication_data:
+        key = doc["molecule_chembl_id"]
+        restr_dict.setdefault(key, []).append(doc)
+    restr_dict = unlist(restr_dict)
+    return restr_dict
+
+
+def restructure_mechanisms(mechanism_data):
+    """ Group mechanism data by molecule_chembl_id
+    """
+    restr_dict = {}
+    for doc in mechanism_data:
+        key = doc["molecule_chembl_id"]
+        restr_dict.setdefault(key, []).append(doc)
+    restr_dict = unlist(restr_dict)
+    return restr_dict
+
+
+def restructure_metabolisms(metabolism_data):
+    """ Group metabolism data by molecule_chembl_id
+    """
+    restr_dict = {}
+    for doc in metabolism_data:
+        key = doc["drug_chembl_id"]
+        restr_dict.setdefault(key, []).append(doc)
+    restr_dict = unlist(restr_dict)
+    return restr_dict
+
 
 def restructure_xref(xref_list):
     """
@@ -42,7 +125,7 @@ def restructure_xref(xref_list):
 def restructure_dict(dictionary):
     restr_dict = dict()
     _flag = 0
-    for key in list(dictionary): # this is for 1
+    for key in list(dictionary):  # this is for 1
         if key == 'molecule_chembl_id':
             restr_dict['_id']=dictionary[key]
         if key == 'molecule_structures' and type(dictionary['molecule_structures'])==dict:
@@ -78,3 +161,9 @@ def restructure_dict(dictionary):
         "therapeutic_flag","med_chem_friendly","molecule_properties.ro3_pass"])
     return restr_dict
 
+
+if __name__ == "__main__":
+    data = load_data("/home/ravila/.data/biothings_studio/datasources/chembl/ChEMBL_27")
+    for doc in data:
+        pass
+        #print(json.dumps(doc, indent=2))
